59669f2be825f2310966f326634a53b00dacfc3c
Thierry Kaoneka <kaonekat@gmail.com>
2025-11-09 01:43:35 +0200

    Viikko 2 tehtävä 4

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,29 +1,13 @@
-import requests
-from player import Player
+from player import Player, PlayerStats, PlayerReader
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
-
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    print("Players from FIN:")
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in players:
-        print(f"{player.name} team {player.team} goals {player.goals} assists {player.assists}")
-    
-    players_sorted = sorted(players, key=lambda x: x.goals + x.assists, reverse=True)
-    print("Players from FIN (sorted by G/A):")
+        print(player)
 
-    for player in players_sorted:
-        print(f"{player.name:25} {player.team:15} {player.goals} + {player.assists} = {player.goals + player.assists}")
- 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,22 @@
+import requests
+
+class PlayerReader:
+    def __init__(self, url):
+        self.response = requests.get(url).json()
+
+    def get_players(self):
+        players = []
+        for player_dict in self.response:
+            players.append(Player(player_dict))
+        return players
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nation):
+        return list(sorted(filter(lambda x: x.nationality == nation, self.players), key=lambda x: x.goals + x.assists, reverse=True))
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
@@ -8,4 +27,4 @@ class Player:
         self.games = dict['games']
     
     def __str__(self):
-        return self.name
+        return f"{self.name:25} {self.team:15} {self.goals} + {self.assists} = {self.goals + self.assists}"
