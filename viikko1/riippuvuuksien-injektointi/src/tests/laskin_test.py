793c88bb8e33b572850209f5f57b828233052c77
Thierry Kaoneka <kaonekat@gmail.com>
2025-11-09 00:11:15 +0200

    Viikko 2 Tehtävät 2 ja 3

---
+++ viikko1/riippuvuuksien-injektointi/src/tests/laskin_test.py
@@ -0,0 +1,37 @@
+import unittest
+from laskin import Laskin
+
+
+class StubIO:
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.outputs = []
+
+    def lue(self, teksti):
+        return self.inputs.pop(0)
+
+    def kirjoita(self, teksti):
+        self.outputs.append(teksti)
+
+
+class TestLaskin(unittest.TestCase):
+    def test_yksi_summa_oikein(self):
+        io = StubIO(["1", "3", "-9999"])
+        laskin = Laskin(io)
+        laskin.suorita()
+
+        self.assertEqual(io.outputs[0], "Summa: 4")
+
+    def test_kaksi_perakkaista_laskua(self):
+
+        io = StubIO(["2", "2", "-9999"])
+        laskin = Laskin(io)
+        laskin.suorita()
+
+
+        io2 = StubIO(["5", "5", "-9999"])
+        laskin2 = Laskin(io2)
+        laskin2.suorita()
+
+        self.assertEqual(io.outputs[0], "Summa: 4")
+        self.assertEqual(io2.outputs[0], "Summa: 10")
