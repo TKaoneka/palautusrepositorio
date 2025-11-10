4c78e21f58a77ae729f13de115c9ae904e8bdf78
Thierry Kaoneka <kaonekat@gmail.com>
2025-11-10 22:28:23 +0200

    viikko 2 teht 8

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,6 +1,6 @@
-from player import Player, PlayerStats, PlayerReader
 from rich.console import Console
 from rich.prompt import Prompt
+from player import PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
@@ -8,10 +8,13 @@ def main():
     stats = PlayerStats(reader)
 
     console = Console()
-    season = Prompt.ask("Season [bold magenta][2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/2025-26][/bold magenta] [bold cyan](2024-25)[/bold cyan]")
-    nationality = Prompt.ask("Nationality [bold magenta][USA/FIN/CAN/SWE/CZE/RUS/SLO/FRA/GBR/SVK/DEN/NED/AUT/BLR/GER/SUI/NOR/UZB/LAT/AUS][/bold magenta]")
-    players = stats.top_scorers_by_nationality(nationality)
-    console.print(players)
+    while True:
+        nationality = Prompt.ask("Nationality [bold magenta][USA/FIN/CAN/SWE/CZE/RUS/SLO/FRA/GBR/SVK/DEN/NED/AUT/BLR/GER/SUI/NOR/UZB/LAT/AUS][/bold magenta]")
+        if nationality == "":
+            break
+
+        players = stats.top_scorers_by_nationality(nationality)
+        console.print(players)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -3,7 +3,7 @@ from rich.table import Table
 
 class PlayerReader:
     def __init__(self, url):
-        self.response = requests.get(url).json()
+        self.response = requests.get(url, timeout=10).json()
 
     def get_players(self):
         players = []
@@ -23,20 +23,20 @@ class PlayerStats:
         table.add_column("goals", style="green")
         table.add_column("assists", style="green")
         table.add_column("points", style="green")
- 
+
         for player in nation_players:
             table.add_row(player.name, player.team, f"{player.goals}", f"{player.assists}", f"{player.goals + player.assists}")
 
-        return table 
+        return table
 
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-        self.nationality = dict['nationality']
-        self.assists = dict['assists']
-        self.goals = dict['goals']
-        self.team = dict['team']
-        self.games = dict['games']
-    
+    def __init__(self, dictionary):
+        self.name = dictionary['name']
+        self.nationality = dictionary['nationality']
+        self.assists = dictionary['assists']
+        self.goals = dictionary['goals']
+        self.team = dictionary['team']
+        self.games = dictionary['games']
+
     def __str__(self):
         return f"{self.name:25} {self.team:15} {self.goals} + {self.assists} = {self.goals + self.assists}"
